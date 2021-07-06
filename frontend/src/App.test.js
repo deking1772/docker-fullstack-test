d060de8a26e15176bdc9ec1c7e4066c5edefd278
deking1772 <deking1772@gmail.com>
2021-07-06 14:03:58 +0900

    travis edit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,5 +2,5 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  consolr.log("test success");
+  console.log("test success");
 });
